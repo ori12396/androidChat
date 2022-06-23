3fecb5e765b0ec5986ddd1c47a4df7ab1ab9269c
ori12396 <93042166+ori12396@users.noreply.github.com>
2022-06-23 23:46:30 +0300

    Add files via upload

---
+++ app/build/generated/ap_generated_sources/debug/out/com/example/mychat/AppDB_Impl.java
@@ -0,0 +1,168 @@
+package com.example.mychat;
+
+import androidx.annotation.NonNull;
+import androidx.room.DatabaseConfiguration;
+import androidx.room.InvalidationTracker;
+import androidx.room.RoomOpenHelper;
+import androidx.room.RoomOpenHelper.Delegate;
+import androidx.room.RoomOpenHelper.ValidationResult;
+import androidx.room.migration.AutoMigrationSpec;
+import androidx.room.migration.Migration;
+import androidx.room.util.DBUtil;
+import androidx.room.util.TableInfo;
+import androidx.room.util.TableInfo.Column;
+import androidx.room.util.TableInfo.ForeignKey;
+import androidx.room.util.TableInfo.Index;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+import androidx.sqlite.db.SupportSQLiteOpenHelper;
+import androidx.sqlite.db.SupportSQLiteOpenHelper.Callback;
+import androidx.sqlite.db.SupportSQLiteOpenHelper.Configuration;
+import java.lang.Class;
+import java.lang.Override;
+import java.lang.String;
+import java.lang.SuppressWarnings;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+@SuppressWarnings({"unchecked", "deprecation"})
+public final class AppDB_Impl extends AppDB {
+  private volatile ContactsDao _contactsDao;
+
+  @Override
+  protected SupportSQLiteOpenHelper createOpenHelper(DatabaseConfiguration configuration) {
+    final SupportSQLiteOpenHelper.Callback _openCallback = new RoomOpenHelper(configuration, new RoomOpenHelper.Delegate(1) {
+      @Override
+      public void createAllTables(SupportSQLiteDatabase _db) {
+        _db.execSQL("CREATE TABLE IF NOT EXISTS `Contact` (`id` TEXT NOT NULL, `name` TEXT, `server` TEXT, `last` TEXT, `lastdate` TEXT, PRIMARY KEY(`id`))");
+        _db.execSQL("CREATE TABLE IF NOT EXISTS room_master_table (id INTEGER PRIMARY KEY,identity_hash TEXT)");
+        _db.execSQL("INSERT OR REPLACE INTO room_master_table (id,identity_hash) VALUES(42, 'b8a988966e3fe214ebd1a6ef39d9c9d8')");
+      }
+
+      @Override
+      public void dropAllTables(SupportSQLiteDatabase _db) {
+        _db.execSQL("DROP TABLE IF EXISTS `Contact`");
+        if (mCallbacks != null) {
+          for (int _i = 0, _size = mCallbacks.size(); _i < _size; _i++) {
+            mCallbacks.get(_i).onDestructiveMigration(_db);
+          }
+        }
+      }
+
+      @Override
+      protected void onCreate(SupportSQLiteDatabase _db) {
+        if (mCallbacks != null) {
+          for (int _i = 0, _size = mCallbacks.size(); _i < _size; _i++) {
+            mCallbacks.get(_i).onCreate(_db);
+          }
+        }
+      }
+
+      @Override
+      public void onOpen(SupportSQLiteDatabase _db) {
+        mDatabase = _db;
+        internalInitInvalidationTracker(_db);
+        if (mCallbacks != null) {
+          for (int _i = 0, _size = mCallbacks.size(); _i < _size; _i++) {
+            mCallbacks.get(_i).onOpen(_db);
+          }
+        }
+      }
+
+      @Override
+      public void onPreMigrate(SupportSQLiteDatabase _db) {
+        DBUtil.dropFtsSyncTriggers(_db);
+      }
+
+      @Override
+      public void onPostMigrate(SupportSQLiteDatabase _db) {
+      }
+
+      @Override
+      protected RoomOpenHelper.ValidationResult onValidateSchema(SupportSQLiteDatabase _db) {
+        final HashMap<String, TableInfo.Column> _columnsContact = new HashMap<String, TableInfo.Column>(5);
+        _columnsContact.put("id", new TableInfo.Column("id", "TEXT", true, 1, null, TableInfo.CREATED_FROM_ENTITY));
+        _columnsContact.put("name", new TableInfo.Column("name", "TEXT", false, 0, null, TableInfo.CREATED_FROM_ENTITY));
+        _columnsContact.put("server", new TableInfo.Column("server", "TEXT", false, 0, null, TableInfo.CREATED_FROM_ENTITY));
+        _columnsContact.put("last", new TableInfo.Column("last", "TEXT", false, 0, null, TableInfo.CREATED_FROM_ENTITY));
+        _columnsContact.put("lastdate", new TableInfo.Column("lastdate", "TEXT", false, 0, null, TableInfo.CREATED_FROM_ENTITY));
+        final HashSet<TableInfo.ForeignKey> _foreignKeysContact = new HashSet<TableInfo.ForeignKey>(0);
+        final HashSet<TableInfo.Index> _indicesContact = new HashSet<TableInfo.Index>(0);
+        final TableInfo _infoContact = new TableInfo("Contact", _columnsContact, _foreignKeysContact, _indicesContact);
+        final TableInfo _existingContact = TableInfo.read(_db, "Contact");
+        if (! _infoContact.equals(_existingContact)) {
+          return new RoomOpenHelper.ValidationResult(false, "Contact(com.example.mychat.Contact).\n"
+                  + " Expected:\n" + _infoContact + "\n"
+                  + " Found:\n" + _existingContact);
+        }
+        return new RoomOpenHelper.ValidationResult(true, null);
+      }
+    }, "b8a988966e3fe214ebd1a6ef39d9c9d8", "2e7fec5f6d28f40d2f43e5766dc9aea8");
+    final SupportSQLiteOpenHelper.Configuration _sqliteConfig = SupportSQLiteOpenHelper.Configuration.builder(configuration.context)
+        .name(configuration.name)
+        .callback(_openCallback)
+        .build();
+    final SupportSQLiteOpenHelper _helper = configuration.sqliteOpenHelperFactory.create(_sqliteConfig);
+    return _helper;
+  }
+
+  @Override
+  protected InvalidationTracker createInvalidationTracker() {
+    final HashMap<String, String> _shadowTablesMap = new HashMap<String, String>(0);
+    HashMap<String, Set<String>> _viewTables = new HashMap<String, Set<String>>(0);
+    return new InvalidationTracker(this, _shadowTablesMap, _viewTables, "Contact");
+  }
+
+  @Override
+  public void clearAllTables() {
+    super.assertNotMainThread();
+    final SupportSQLiteDatabase _db = super.getOpenHelper().getWritableDatabase();
+    try {
+      super.beginTransaction();
+      _db.execSQL("DELETE FROM `Contact`");
+      super.setTransactionSuccessful();
+    } finally {
+      super.endTransaction();
+      _db.query("PRAGMA wal_checkpoint(FULL)").close();
+      if (!_db.inTransaction()) {
+        _db.execSQL("VACUUM");
+      }
+    }
+  }
+
+  @Override
+  protected Map<Class<?>, List<Class<?>>> getRequiredTypeConverters() {
+    final HashMap<Class<?>, List<Class<?>>> _typeConvertersMap = new HashMap<Class<?>, List<Class<?>>>();
+    _typeConvertersMap.put(ContactsDao.class, ContactsDao_Impl.getRequiredConverters());
+    return _typeConvertersMap;
+  }
+
+  @Override
+  public Set<Class<? extends AutoMigrationSpec>> getRequiredAutoMigrationSpecs() {
+    final HashSet<Class<? extends AutoMigrationSpec>> _autoMigrationSpecsSet = new HashSet<Class<? extends AutoMigrationSpec>>();
+    return _autoMigrationSpecsSet;
+  }
+
+  @Override
+  public List<Migration> getAutoMigrations(
+      @NonNull Map<Class<? extends AutoMigrationSpec>, AutoMigrationSpec> autoMigrationSpecsMap) {
+    return Arrays.asList();
+  }
+
+  @Override
+  public ContactsDao contactsDao() {
+    if (_contactsDao != null) {
+      return _contactsDao;
+    } else {
+      synchronized(this) {
+        if(_contactsDao == null) {
+          _contactsDao = new ContactsDao_Impl(this);
+        }
+        return _contactsDao;
+      }
+    }
+  }
+}
---
+++ app/build/generated/ap_generated_sources/debug/out/com/example/mychat/ContactsDao_Impl.java
@@ -0,0 +1,288 @@
+package com.example.mychat;
+
+import android.database.Cursor;
+import androidx.room.EntityDeletionOrUpdateAdapter;
+import androidx.room.EntityInsertionAdapter;
+import androidx.room.RoomDatabase;
+import androidx.room.RoomSQLiteQuery;
+import androidx.room.util.CursorUtil;
+import androidx.room.util.DBUtil;
+import androidx.sqlite.db.SupportSQLiteStatement;
+import java.lang.Class;
+import java.lang.Override;
+import java.lang.String;
+import java.lang.SuppressWarnings;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@SuppressWarnings({"unchecked", "deprecation"})
+public final class ContactsDao_Impl implements ContactsDao {
+  private final RoomDatabase __db;
+
+  private final EntityInsertionAdapter<Contact> __insertionAdapterOfContact;
+
+  private final EntityDeletionOrUpdateAdapter<Contact> __deletionAdapterOfContact;
+
+  private final EntityDeletionOrUpdateAdapter<Contact> __updateAdapterOfContact;
+
+  public ContactsDao_Impl(RoomDatabase __db) {
+    this.__db = __db;
+    this.__insertionAdapterOfContact = new EntityInsertionAdapter<Contact>(__db) {
+      @Override
+      public String createQuery() {
+        return "INSERT OR ABORT INTO `Contact` (`id`,`name`,`server`,`last`,`lastdate`) VALUES (?,?,?,?,?)";
+      }
+
+      @Override
+      public void bind(SupportSQLiteStatement stmt, Contact value) {
+        if (value.getId() == null) {
+          stmt.bindNull(1);
+        } else {
+          stmt.bindString(1, value.getId());
+        }
+        if (value.getName() == null) {
+          stmt.bindNull(2);
+        } else {
+          stmt.bindString(2, value.getName());
+        }
+        if (value.getServer() == null) {
+          stmt.bindNull(3);
+        } else {
+          stmt.bindString(3, value.getServer());
+        }
+        if (value.getLast() == null) {
+          stmt.bindNull(4);
+        } else {
+          stmt.bindString(4, value.getLast());
+        }
+        if (value.getLastdate() == null) {
+          stmt.bindNull(5);
+        } else {
+          stmt.bindString(5, value.getLastdate());
+        }
+      }
+    };
+    this.__deletionAdapterOfContact = new EntityDeletionOrUpdateAdapter<Contact>(__db) {
+      @Override
+      public String createQuery() {
+        return "DELETE FROM `Contact` WHERE `id` = ?";
+      }
+
+      @Override
+      public void bind(SupportSQLiteStatement stmt, Contact value) {
+        if (value.getId() == null) {
+          stmt.bindNull(1);
+        } else {
+          stmt.bindString(1, value.getId());
+        }
+      }
+    };
+    this.__updateAdapterOfContact = new EntityDeletionOrUpdateAdapter<Contact>(__db) {
+      @Override
+      public String createQuery() {
+        return "UPDATE OR ABORT `Contact` SET `id` = ?,`name` = ?,`server` = ?,`last` = ?,`lastdate` = ? WHERE `id` = ?";
+      }
+
+      @Override
+      public void bind(SupportSQLiteStatement stmt, Contact value) {
+        if (value.getId() == null) {
+          stmt.bindNull(1);
+        } else {
+          stmt.bindString(1, value.getId());
+        }
+        if (value.getName() == null) {
+          stmt.bindNull(2);
+        } else {
+          stmt.bindString(2, value.getName());
+        }
+        if (value.getServer() == null) {
+          stmt.bindNull(3);
+        } else {
+          stmt.bindString(3, value.getServer());
+        }
+        if (value.getLast() == null) {
+          stmt.bindNull(4);
+        } else {
+          stmt.bindString(4, value.getLast());
+        }
+        if (value.getLastdate() == null) {
+          stmt.bindNull(5);
+        } else {
+          stmt.bindString(5, value.getLastdate());
+        }
+        if (value.getId() == null) {
+          stmt.bindNull(6);
+        } else {
+          stmt.bindString(6, value.getId());
+        }
+      }
+    };
+  }
+
+  @Override
+  public void insert(final Contact... contacts) {
+    __db.assertNotSuspendingTransaction();
+    __db.beginTransaction();
+    try {
+      __insertionAdapterOfContact.insert(contacts);
+      __db.setTransactionSuccessful();
+    } finally {
+      __db.endTransaction();
+    }
+  }
+
+  @Override
+  public void delete(final Contact... contacts) {
+    __db.assertNotSuspendingTransaction();
+    __db.beginTransaction();
+    try {
+      __deletionAdapterOfContact.handleMultiple(contacts);
+      __db.setTransactionSuccessful();
+    } finally {
+      __db.endTransaction();
+    }
+  }
+
+  @Override
+  public void update(final Contact... contacts) {
+    __db.assertNotSuspendingTransaction();
+    __db.beginTransaction();
+    try {
+      __updateAdapterOfContact.handleMultiple(contacts);
+      __db.setTransactionSuccessful();
+    } finally {
+      __db.endTransaction();
+    }
+  }
+
+  @Override
+  public List<Contact> index() {
+    final String _sql = "SELECT * FROM Contact";
+    final RoomSQLiteQuery _statement = RoomSQLiteQuery.acquire(_sql, 0);
+    __db.assertNotSuspendingTransaction();
+    final Cursor _cursor = DBUtil.query(__db, _statement, false, null);
+    try {
+      final int _cursorIndexOfId = CursorUtil.getColumnIndexOrThrow(_cursor, "id");
+      final int _cursorIndexOfName = CursorUtil.getColumnIndexOrThrow(_cursor, "name");
+      final int _cursorIndexOfServer = CursorUtil.getColumnIndexOrThrow(_cursor, "server");
+      final int _cursorIndexOfLast = CursorUtil.getColumnIndexOrThrow(_cursor, "last");
+      final int _cursorIndexOfLastdate = CursorUtil.getColumnIndexOrThrow(_cursor, "lastdate");
+      final List<Contact> _result = new ArrayList<Contact>(_cursor.getCount());
+      while(_cursor.moveToNext()) {
+        final Contact _item;
+        _item = new Contact();
+        final String _tmpId;
+        if (_cursor.isNull(_cursorIndexOfId)) {
+          _tmpId = null;
+        } else {
+          _tmpId = _cursor.getString(_cursorIndexOfId);
+        }
+        _item.setId(_tmpId);
+        final String _tmpName;
+        if (_cursor.isNull(_cursorIndexOfName)) {
+          _tmpName = null;
+        } else {
+          _tmpName = _cursor.getString(_cursorIndexOfName);
+        }
+        _item.setName(_tmpName);
+        final String _tmpServer;
+        if (_cursor.isNull(_cursorIndexOfServer)) {
+          _tmpServer = null;
+        } else {
+          _tmpServer = _cursor.getString(_cursorIndexOfServer);
+        }
+        _item.setServer(_tmpServer);
+        final String _tmpLast;
+        if (_cursor.isNull(_cursorIndexOfLast)) {
+          _tmpLast = null;
+        } else {
+          _tmpLast = _cursor.getString(_cursorIndexOfLast);
+        }
+        _item.setLast(_tmpLast);
+        final String _tmpLastdate;
+        if (_cursor.isNull(_cursorIndexOfLastdate)) {
+          _tmpLastdate = null;
+        } else {
+          _tmpLastdate = _cursor.getString(_cursorIndexOfLastdate);
+        }
+        _item.setLastdate(_tmpLastdate);
+        _result.add(_item);
+      }
+      return _result;
+    } finally {
+      _cursor.close();
+      _statement.release();
+    }
+  }
+
+  @Override
+  public Contact get(final String id) {
+    final String _sql = "SELECT * FROM Contact WHERE id=?";
+    final RoomSQLiteQuery _statement = RoomSQLiteQuery.acquire(_sql, 1);
+    int _argIndex = 1;
+    if (id == null) {
+      _statement.bindNull(_argIndex);
+    } else {
+      _statement.bindString(_argIndex, id);
+    }
+    __db.assertNotSuspendingTransaction();
+    final Cursor _cursor = DBUtil.query(__db, _statement, false, null);
+    try {
+      final int _cursorIndexOfId = CursorUtil.getColumnIndexOrThrow(_cursor, "id");
+      final int _cursorIndexOfName = CursorUtil.getColumnIndexOrThrow(_cursor, "name");
+      final int _cursorIndexOfServer = CursorUtil.getColumnIndexOrThrow(_cursor, "server");
+      final int _cursorIndexOfLast = CursorUtil.getColumnIndexOrThrow(_cursor, "last");
+      final int _cursorIndexOfLastdate = CursorUtil.getColumnIndexOrThrow(_cursor, "lastdate");
+      final Contact _result;
+      if(_cursor.moveToFirst()) {
+        _result = new Contact();
+        final String _tmpId;
+        if (_cursor.isNull(_cursorIndexOfId)) {
+          _tmpId = null;
+        } else {
+          _tmpId = _cursor.getString(_cursorIndexOfId);
+        }
+        _result.setId(_tmpId);
+        final String _tmpName;
+        if (_cursor.isNull(_cursorIndexOfName)) {
+          _tmpName = null;
+        } else {
+          _tmpName = _cursor.getString(_cursorIndexOfName);
+        }
+        _result.setName(_tmpName);
+        final String _tmpServer;
+        if (_cursor.isNull(_cursorIndexOfServer)) {
+          _tmpServer = null;
+        } else {
+          _tmpServer = _cursor.getString(_cursorIndexOfServer);
+        }
+        _result.setServer(_tmpServer);
+        final String _tmpLast;
+        if (_cursor.isNull(_cursorIndexOfLast)) {
+          _tmpLast = null;
+        } else {
+          _tmpLast = _cursor.getString(_cursorIndexOfLast);
+        }
+        _result.setLast(_tmpLast);
+        final String _tmpLastdate;
+        if (_cursor.isNull(_cursorIndexOfLastdate)) {
+          _tmpLastdate = null;
+        } else {
+          _tmpLastdate = _cursor.getString(_cursorIndexOfLastdate);
+        }
+        _result.setLastdate(_tmpLastdate);
+      } else {
+        _result = null;
+      }
+      return _result;
+    } finally {
+      _cursor.close();
+      _statement.release();
+    }
+  }
+
+  public static List<Class<?>> getRequiredConverters() {
+    return Collections.emptyList();
+  }
+}
